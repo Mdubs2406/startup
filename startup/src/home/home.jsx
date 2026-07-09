ee085bb4b912369c985b95137fc398b1247ec0a6
Matthew Wellman <mdubs2406@gmail.com>
2026-07-08 18:34:34 -0600

    Fix minor typos

--- startup/src/home/home.jsx
+++ startup/src/home/home.jsx
@@ -2,40 +2,40 @@ import React from 'react';
 
 export function Home() {
     return (
-        <main class="py-3">
-                <div class="row justify-content-evenly mb-3">
+        <main className="py-3">
+                <div className="row justify-content-evenly mb-3">
                     {/* Websocket Placeholder. Counts will updated in realtime */}
-                    <div id="deed-count-daily"class="col-auto display-6 border border-light rounded pb-1">
-                        <span class="fw-bold">
+                    <div id="deed-count-daily"className="col-auto display-6 border border-light rounded pb-1">
+                        <span className="fw-bold">
                             3
                         </span> 
                         Good Deeds Today
                     </div>
-                    <div id="deed-count-total" class="col-auto display-6 border border-light rounded pb-1">
-                        <span class="fw-bold">
+                    <div id="deed-count-total" className="col-auto display-6 border border-light rounded pb-1">
+                        <span className="fw-bold">
                             142
                         </span> 
                         Total Good Deeds
                     </div>
                 </div>
 
-                <div class="card w-auto mb-3 mx-2">
-                    <h3 class="card-header text-center">
+                <div className="card w-auto mb-3 mx-2">
+                    <h3 className="card-header text-center">
                         Everyday Kindness
                     </h3>
-                    <div class="card-body text-center">
-                        <h4 class="card-title">
+                    <div className="card-body text-center">
+                        <h4 className="card-title">
                             Hold the door open for a stranger
                         </h4>
-                        <h6 class="card-text text-center">
+                        <h6 className="card-text text-center">
                             A small gesture that takes two seconds, but can brighten someone's entire day.
                         </h6>
                     </div>
                 </div>
 
-                <div class="card mx-2">
-                    <h4 class="card-title text-center pt-2">🔥<span>3</span>-day🔥streak!</h4>
-                    <div class="card-footer text-center">You've done goood <span>3</span> days running. Keep Going!</div>
+                <div className="card mx-2">
+                    <h4 className="card-title text-center pt-2">🔥<span>3</span>-day🔥streak!</h4>
+                    <div className="card-footer text-center">You've done goood <span>3</span> days running. Keep Going!</div>
                 </div>
             </main>
     );
--- startup/src/journal/journal.jsx
+++ startup/src/journal/journal.jsx
@@ -2,46 +2,46 @@ import React from 'react';
 
 export function Journal() {
     return (
-        <main class="py-3">
-                <section id="create-entry" class="card shadow-sm mb-3 mx-2">
-                    <h3 class="card-header">Create a Journal Entry</h3>
-                    <div class="card-body">
+        <main className="py-3">
+                <section id="create-entry" className="card shadow-sm mb-3 mx-2">
+                    <h3 className="card-header">Create a Journal Entry</h3>
+                    <div className="card-body">
                         <form method="post" action="journal.html">
-                            <div class="input-group mb-2">
-                                <span class="input-group-text">Date</span>
+                            <div className="input-group mb-2">
+                                <span className="input-group-text">Date</span>
                                 <input 
                                     type="date" 
                                     id="entry-date" 
                                     name="entry-date"
-                                    class="form-control"
+                                    className="form-control"
                                     required />
-                                <span class="input-group-text">Time</span>
+                                <span className="input-group-text">Time</span>
                                 <input 
                                     type="time" 
                                     name="entry-time" 
                                     id="entry-time"
-                                    class="form-control"
+                                    className="form-control"
                                     required />
                             </div>
-                            <div class="mb-2">
+                            <div className="mb-2">
                                 <textarea 
                                     name="entry-desc" 
                                     id="entry-desc" 
                                     placeholder="How did it go?"
                                     rows="5"
-                                    class="form-control"></textarea>
+                                    className="form-control"></textarea>
                             </div>
-                            <button type="submit" class="btn btn-primary mx-1">Record Entry</button>
-                            <button type="reset" class="btn btn-secondary mx-1">Clear</button>
+                            <button type="submit" className="btn btn-primary mx-1">Record Entry</button>
+                            <button type="reset" className="btn btn-secondary mx-1">Clear</button>
                         </form>
                     </div>
                 </section>
 
-                <section id="entries-display" class="card mx-2">
+                <section id="entries-display" className="card mx-2">
                     {/* DB Placeholder. Will recall journal entries from DB */}
-                <h3 class="card-header">A Record of my Accomplishments</h3>
-                <table class="card-body table table-striped-columns">
-                    <thead class="table-light">
+                <h3 className="card-header">A Record of my Accomplishments</h3>
+                <table className="card-body table table-striped-columns">
+                    <thead className="table-light">
                         <tr>
                             <th>Date</th>
                             <th>Time</th>
