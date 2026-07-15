cb3085c2bb1ca5831b8a24a283c340376da156b3
Matthew Wellman <mdubs2406@gmail.com>
2026-07-14 20:08:00 -0600

    Add sign out functionally, and navigate to home button

--- startup/src/login/userPortal.jsx
+++ startup/src/login/userPortal.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-import { useNavigation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
 export function userPortal(props) {
-    
+    const nav = useNavigate();
+
+    function signOut() {
+        localStorage.removeItem('email');
+        props.onSignOut();
+    }
+
+    return (
+        <div>
+            <div>Hello, {props.email}. We hope you having a wonderful day!</div>
+            <Button variant='primary' onClick={() => nav('/home')}>
+                Explore
+            </Button>
+            <Button variant='secondary' conClick={() => signOut()}>
+                Sign Out
+            </Button>
+        </div>
+    );
 }
