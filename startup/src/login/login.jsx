efd9de65271b5354e24d2e078607a835c6665118
Matthew Wellman <mdubs2406@gmail.com>
2026-07-15 17:01:01 -0600

    Add display for various access states to login

--- startup/src/login/login.jsx
+++ startup/src/login/login.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { access } from './acces';
 import { onHold } from './onHold';
 import { userPortal } from './userPortal';
+import { Access } from './access';
 
-export function Login({email, access, updateAccess}) {
+export function Login({email, password, access, updateAccess}) {
     return (
         <main className="py-3 text-center">
-                
+            <div>
+                {access === access.unkown && <h1>Preparing to do good . . .</h1>}
+                {access === access.onHold && 
+                    <onHold email={email} password={password} onSignIn={(userEmail) => 
+                        updateAccess(userEmail, Access.Granted)} 
+                />}
+                {access === access.Granted && <userPortal email={email} onSignOut={() => 
+                    updateAccess(email, Access.onHold)} 
+                />}
+            </div>
         </main>
     );
 }
