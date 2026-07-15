47fdb9c607ce6d6cd17142055138632f59082749
Matthew Wellman <mdubs2406@gmail.com>
2026-07-14 19:42:49 -0600

    Add base HTML to be returned by onHold.jsx

--- startup/src/login/onHold.jsx
+++ startup/src/login/onHold.jsx
@@ -18,4 +18,50 @@ export function onHold(props) {
         localStorage.setItem('email', email);
         props.onLogin(email);
     }
+
+    return (
+        <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <form method="post" action="home.html">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">Email</span>
+                                <input 
+                                    type="email" 
+                                    id="email" 
+                                    name="email" 
+                                    placeholder="email@example.com"
+                                    required
+                                    className="form-control" />
+                            </div>
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">Password</span>
+                                <input 
+                                    type="password" 
+                                    id="password" 
+                                    name="password" 
+                                    placeholder="********"
+                                    required
+                                    className="form-control"/>
+                            </div>
+
+                            <div>
+                                <button 
+                                    value="login" 
+                                    name="submit" 
+                                    type="submit" 
+                                    className="btn btn-primary mx-1">
+                                    Submit
+                                </button>
+                                <button 
+                                    value="create" 
+                                    name="create" 
+                                    type="submit" 
+                                    className="btn btn-secondary mx-1">
+                                    Create Account
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+    )
 }
