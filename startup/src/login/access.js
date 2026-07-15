13c4889d925346453392ddf539e6c4a4c24cb08c
Matthew Wellman <mdubs2406@gmail.com>
2026-07-14 19:27:44 -0600

    Adjust naming conventions in Access class

--- startup/src/login/access.js
+++ startup/src/login/access.js
@@ -1,7 +1,7 @@
 export class Access {
     static Unkown = new Access('unkown');
     static Granted = new Access('granted');
-    static Hold = new Access('hold');
+    static Hold = new Access('onHold');
 
     constructor(state) {
         this.state = state;
