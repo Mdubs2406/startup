6752d8a7abc3bc81644fccdccc1657a5884637c9
Matthew Wellman <mdubs2406@gmail.com>
2026-07-15 19:56:34 -0600

    Prep Home.jsx for use of variables update via React

--- startup/src/home/home.jsx
+++ startup/src/home/home.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 export function Home() {
+    const [deed, setDeed] = React.useState('Awaiting Inspiration...');
+    const [inspo, setInpo] = React.useState('...something great is coming!');
+    {/*These will access a data base later, placeholder for now*/}
+    const [streak, setStreak] = React.useState(localStorage.getItem('streak') || 0);
+    const [dayCount, setDayCount] = React.useState(localStorage.getItem('dayCount') || 7);
+    const [totalCount, setTotalCount] = React.useState(localStorage.getItem('totalCount') || 107);
+    const [lastCompleted, setLastCompleted] = React.useState(localStorage.getItem('lastCompleted') || null);
+    
+
+    React.useEffect(() => {
+        {/*These will request from DB*/}
+        setDeed('Hold the door open for a stranger');
+        setInpo('A small gesture that takes two seconds, but can brighten someone\'s entire day.')
+    }, []);
+
     return (
         <main className="py-3">
                 <div className="row justify-content-evenly mb-3">
                     {/* Websocket Placeholder. Counts will updated in realtime */}
                     <div id="deed-count-daily"className="col-auto display-6 border border-light rounded pb-1">
-                        <span className="fw-bold">
-                            3
-                        </span> 
+                        <span className="fw-bold me-3">{dayCount}</span> 
                         Good Deeds Today
                     </div>
                     <div id="deed-count-total" className="col-auto display-6 border border-light rounded pb-1">
-                        <span className="fw-bold">
-                            142
-                        </span> 
+                        <span className=" me-3">{totalCount}</span> 
                         Total Good Deeds
                     </div>
                 </div>
@@ -24,13 +36,13 @@ export function Home() {
                         Everyday Kindness
                     </h3>
                     <div className="card-body text-center">
-                        <h4 className="card-title">
-                            Hold the door open for a stranger
-                        </h4>
-                        <h6 className="card-text text-center">
-                            A small gesture that takes two seconds, but can brighten someone's entire day.
-                        </h6>
+                        <h4 className="card-title">{deed}</h4>
+                        <h6 className="card-text text-center mb-3">{inspo}</h6>
+                        <Button>
+                            <button type="submit" className="btn btn-primary mx-1">I did it!</button>
+                        </Button>
                     </div>
+                    <div className="card-footer"></div>
                 </div>
 
                 <div className="card mx-2">
