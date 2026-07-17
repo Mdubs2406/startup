4e710abc1f18ee0de4c19ba485d8861f44ec0ffe
Matthew Wellman <mdubs2406@gmail.com>
2026-07-16 21:35:24 -0600

    Update white spacing

--- startup/src/home/home.jsx
+++ startup/src/home/home.jsx
@@ -51,34 +51,34 @@ export function Home() {
 
     return (
         <main className="py-3">
-                <div className="row justify-content-evenly mb-3">
-                    {/* Websocket Placeholder. Counts will updated in realtime */}
-                    <div id="deed-count-daily"className="col-auto display-6 border border-light rounded pb-1">
-                        <span className="fw-bold me-3">{globalStats.dayCount}</span> 
-                        Good Deeds Today
-                    </div>
-                    <div id="deed-count-total" className="col-auto display-6 border border-light rounded pb-1">
-                        <span className=" me-3">{globalStats.totalCount}</span> 
-                        Total Good Deeds
-                    </div>
+            <div className="row justify-content-evenly mb-3">
+                {/* Websocket Placeholder. Counts will updated in realtime */}
+                <div id="deed-count-daily"className="col-auto display-6 border border-light rounded pb-1">
+                    <span className="fw-bold me-3">{globalStats.dayCount}</span> 
+                    Good Deeds Today
                 </div>
+                <div id="deed-count-total" className="col-auto display-6 border border-light rounded pb-1">
+                    <span className=" me-3">{globalStats.totalCount}</span> 
+                    Total Good Deeds
+                </div>
+            </div>
 
-                <div className="card w-auto mb-3 mx-2">
-                    <h3 className="card-header text-center">
-                        Everyday Kindness
-                    </h3>
-                    <div className="card-body text-center">
-                        <h4 className="card-title">{deed}</h4>
-                        <h6 className="card-text text-center mb-3">{inspo}</h6>
-                        <CompleteDeed globalStats={globalStats} setGlobalStats={setGlobalStats} userStats={userStats} setUserStats={setUserStats} />
-                    </div>
-                    <div className="card-footer"></div>
+            <div className="card w-auto mb-3 mx-2">
+                <h3 className="card-header text-center">
+                    Everyday Kindness
+                </h3>
+                <div className="card-body text-center">
+                    <h4 className="card-title">{deed}</h4>
+                    <h6 className="card-text text-center mb-3">{inspo}</h6>
+                    <CompleteDeed globalStats={globalStats} setGlobalStats={setGlobalStats} userStats={userStats} setUserStats={setUserStats} />
                 </div>
+                <div className="card-footer"></div>
+            </div>
 
-                <div className="card mx-2">
-                    {userStats.streak !== 0 && <DisplayStreak streak={userStats.streak} />}
-                    {userStats.streak === 0 && <NoStreak />}
-                </div>
-            </main>
+            <div className="card mx-2">
+                {userStats.streak !== 0 && <DisplayStreak streak={userStats.streak} />}
+                {userStats.streak === 0 && <NoStreak />}
+            </div>
+        </main>
     );
 };
