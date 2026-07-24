111152ed5e08b73eb20b90e26ac71ba0b86da96a
Matthew Wellman <mdubs2406@gmail.com>
2026-07-24 14:33:06 -0600

    Add API fetching for journal data

--- startup/src/journal/journal.jsx
+++ startup/src/journal/journal.jsx
@@ -3,17 +3,32 @@ import { NewEntry } from './newEntry';
 import { DisplayJournal } from './displayJournal';
 
 export function Journal() {
-  const [journal, setJournal] = React.useState(() => {
-    return JSON.parse(localStorage.getItem("journal")) || [];
-  });
+  const [journal, setJournal] = React.useState([]);
+  const [errorMsg, setErrorMsg] = React.useState(null);
+
   React.useEffect(() => {
-    localStorage.setItem("journal", JSON.stringify(journal));
-  }, [journal]);
+    fetch(`/api/journal`)
+      .then(async (res) => {
+        if (!res.ok) {
+          const err = await res.json();
+          throw new Error(err.msg || 'Could not retrieve journal entries');
+        }
+
+        return res.json();
+      })
+      .then((data) => {
+        setJournal(data)
+      })
+      .catch((error) => {
+        setErrorMsg(error.message);
+      });
+  }, []);
 
   return (
     <main className="py-3">
       <NewEntry setJournal={setJournal} />
       <DisplayJournal journal={journal} />
+      <ErrorDisplay msg={errorMsg} dismiss={() => setErrorMsg(null)} />
     </main>
   );
 }
