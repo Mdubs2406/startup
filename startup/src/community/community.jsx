9d546e42311420c2eec451e9c7be4bb0e72e83db
Matthew Wellman <mdubs2406@gmail.com>
2026-07-24 11:46:09 -0600

    Add fetching of community post list from backend

--- startup/src/community/community.jsx
+++ startup/src/community/community.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import { MakePost } from './makePost';
 import { MakeTable } from './makeTable';
+import { ErrorDisplay } from '../notification/errorDisplay';
 
 export function Community({ setShow }) {
     const [comPosts, setComPosts] = React.useState([]);
+    const [errorMsg, setErrorMsg] = React.useState(null);
 
-    async function getPost() {
-        
-    }
+    React.useEffect(() => {
+        fetch(`/api/community`)
+        .then(async (res) => {
+            if (!res.ok) {
+                const err = await res.json();
+                throw new Error(err.msg || 'Could not get posts');
+            }
+
+            return res.json();
+        })
+        .then((data) => {
+            setComPosts(data)
+        })
+        .catch((error) => {
+            setErrorMsg(error.message);
+        });
+    }, []);
     
     return (
         <main className="py-3">
-            <MakePost setComPosts={setComPosts}/>
+            <MakePost setComPosts={setComPosts} setShow={setShow}/>
             <MakeTable comPosts={comPosts}/>
+            <ErrorDisplay msg={errorMsg} dismiss={() => setErrorMsg(null)} />
         </main>
     );
 }
