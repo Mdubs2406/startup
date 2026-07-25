ba679145a3cebbf0c4fa4327fab6d4bd9f32f22a
Matthew Wellman <mdubs2406@gmail.com>
2026-07-25 11:54:59 -0600

    Update error messages to be more descriptive

--- startup/service/index.js
+++ startup/service/index.js
@@ -48,14 +48,19 @@ apiRouter.post('/users/create', async (req, res) => {
 apiRouter.post('/users/signin', async (req, res) => {
   const user = await findAccount('email', req.body.email);
 
-  if (user && (await bcrypt.compare(req.body.password, user.password))) {
-    user.authToken = uuid.v4();
-    setCookie(res, user.authToken);
-    res.send({ email: user.email });
+  if (!user) {
+    res.status(404).send({ msg: 'Account not found. Please create an account.'});
     return;
-  } else {
-    res.status(401).send({ msg: 'Incorrect Email or Password.' });
   }
+
+  if (!(await bcrypt.compare(req.body.password, user.password))) {
+      res.status(401).send({ msg: 'Incorrect Email or Password.' });
+      return;
+    }
+
+  user.authToken = uuid.v4();
+  setCookie(res, user.authToken);
+  res.send({ email: user.email });
 });
 
 apiRouter.delete('/users/signout', async (req, res) => {
