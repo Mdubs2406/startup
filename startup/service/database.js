7744f6450aba492d8a3a47ce6ca342719fcdc992
Matthew Wellman <mdubs2406@gmail.com>
2026-07-26 15:04:29 -0600

    Import and setup service to access mongodb

---
+++ startup/service/database.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+
+const userCollection = db.collection('user');
+const postCollection = db.collection('post');
+const journalCollection = db.collection('journal');
+
+// For connection error handling and testing
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connected to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
