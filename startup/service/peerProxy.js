ca1c81796932f97f14bd16a8f6232f25ebed8afb
Matthew Wellman <mdubs2406@gmail.com>
2026-07-30 17:26:28 -0600

    Setup wsServer and send message functionality

--- startup/service/peerProxy.js
+++ startup/service/peerProxy.js
@@ -1,5 +1,23 @@
 const {WebSocketServer, webSocket } = require('ws');
 
-function peerProxy(httpServer) {}
+function seUpWebSocket(httpServer) {
+ const wsServer = new WebSocketServer({ server: httpServer });
+
+ wsServer.on('connection', (connection) => {
+  connection.alive = true;
+
+  connection.on('pong', () => {
+    connection.alive = true;
+  });
+
+  connection.on('message', (message) => {
+    for (const otherConnection of wsServer.clients) {
+      if (otherConnection !== connection && otherConnection.readyState === 1) {
+        otherConnection.send(message);
+      }
+    }
+  });
+ });
+}
 
 module.exports = { peerProxy };
